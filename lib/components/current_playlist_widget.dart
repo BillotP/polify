0b20914f7c16d73b6f01314e983518111a380f6a
ravachol nildock <ravacholnildock@devnull.mail>
2023-08-27 12:38:43 +0200

    feat(playlist): wip playlist view

--- lib/components/current_playlist_widget.dart
+++ lib/components/current_playlist_widget.dart
@@ -14,12 +14,47 @@ class _CurrentPlaylistWidgetState extends State<CurrentPlaylistWidget> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView.builder(
-        itemCount: pls.currentPlaylist.length,
-        itemBuilder: (context, index) => ListTile(
-          dense: true,
-          title: Text(
-              pls.currentPlaylist.elementAt(index).title ?? "Unknow title"),
+      backgroundColor: Colors.black,
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const Text("Current Playlist"),
+            Text("Titles : ${pls.currentPlaylist.length}"),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(Icons.save),
+                ),
+                IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(Icons.offline_pin_rounded),
+                ),
+                IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(Icons.delete_forever_outlined),
+                )
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: pls.currentPlaylist.length,
+                itemBuilder: (context, index) => ListTile(
+                  dense: true,
+                  onTap: () async =>
+                      {await pls.playIndexSong(index), setState(() {})},
+                  leading: const Icon(Icons.playlist_add_check_circle),
+                  trailing: pls.playIndex == index
+                      ? const Icon(Icons.headset_rounded)
+                      : null,
+                  title: Text(pls.currentPlaylist.elementAt(index).title ??
+                      "Unknow title"),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
