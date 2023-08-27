f36674ba8027d68d9f57fc5ae52cfc882b7d33ab
ravachol nildock <ravacholnildock@devnull.mail>
2023-08-27 12:38:06 +0200

    feat(bucket): Wip bucket tile

--- lib/components/bucket_tile.dart
+++ lib/components/bucket_tile.dart
@@ -27,3 +27,58 @@ Widget bucketListTile(MusicBucket bucket, bool loading,
       onTap: () => Get.to(BucketWidget(bucket: bucket)),
       title: Text(bucket.name),
     );
+
+Widget bucketCardTile(MusicBucket bucket, bool loading,
+        void Function(MusicBucket bucket) onRefreshBucketContent) =>
+    Card(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Expanded(
+            child: IconButton(
+                iconSize: 100,
+                splashRadius: 100,
+                onPressed: () => Get.to(BucketWidget(bucket: bucket)),
+                icon: const Icon(Icons.storage_rounded)),
+          ),
+          Expanded(
+            child: Center(
+              child: IconButton(
+                onPressed: () => Get.to(BucketWidget(bucket: bucket)),
+                icon: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      bucket.name,
+                      style: const TextStyle(
+                          color: Colors.black, backgroundColor: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      bucket.endpoint,
+                      style: const TextStyle(
+                          color: Colors.black, backgroundColor: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              loading
+                  ? Transform.scale(
+                      scale: 0.5,
+                      child: const CircularProgressIndicator(),
+                    )
+                  : IconButton(
+                      onPressed: () => onRefreshBucketContent(bucket),
+                      icon: const Icon(Icons.refresh)),
+            ],
+          ),
+        ],
+      ),
+    );
