2bc74fc6b8baaa08f58b3fac7a6c3efb280ae874
ravachol nildock <ravacholnildock@devnull.mail>
2023-08-29 11:23:06 +0200

    feat(playlist): Reordable playlist

--- lib/components/current_playlist_widget.dart
+++ lib/components/current_playlist_widget.dart
@@ -11,47 +11,105 @@ class CurrentPlaylistWidget extends StatefulWidget {
 
 class _CurrentPlaylistWidgetState extends State<CurrentPlaylistWidget> {
   PlayerService pls = Get.find();
+  // Row(
+  //   mainAxisSize: MainAxisSize.max,
+  //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //   children: [
+  //     Text("Current Playlist Titles : ${pls.currentPlaylist.length}"),
+  //     Row(
+  //       mainAxisSize: MainAxisSize.min,
+  //       children: [
+  //         IconButton(
+  //           onPressed: () => {},
+  //           tooltip: "Save",
+  //           color: Colors.white,
+  //           icon: const Icon(Icons.save),
+  //         ),
+  //         IconButton(
+  //           onPressed: () => {},
+  //           color: Colors.white,
+  //           tooltip: "Clear",
+  //           icon: const Icon(Icons.delete_forever_outlined),
+  //         )
+  //       ],
+  //     ),
+  //   ],
+  // ),
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        const Text("Current Playlist"),
-        Text("Titles : ${pls.currentPlaylist.length}"),
-        Row(
+    if (pls.currentPlaylist.isEmpty) {
+      return const Center(
+        child: Text("Go listen to some music !"),
+      );
+    }
+    return ReorderableListView.builder(
+      buildDefaultDragHandles: false,
+      onReorder: (oldIndex, newIndex) {
+        setState(() {
+          if (oldIndex < newIndex) {
+            newIndex -= 1;
+          }
+          final SongToplay item = pls.currentPlaylist.removeAt(oldIndex);
+          pls.currentPlaylist.insert(newIndex, item);
+        });
+      },
+      itemCount: pls.currentPlaylist.length,
+      itemBuilder: (context, index) => ListTile(
+        // tileColor: Colors.orange,
+        dense: true,
+        trailing: ReorderableDragStartListener(
+            index: index,
+            child: const Icon(
+              Icons.drag_indicator_outlined,
+              color: Colors.white,
+            )),
+        key: Key(pls.currentPlaylist[index].songId.toString()),
+        leading: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
             IconButton(
-              onPressed: () => {},
-              icon: const Icon(Icons.save),
+              icon: const Icon(Icons.remove_circle),
+              color: Colors.white,
+              onPressed: () => {
+                pls.currentPlaylist.removeWhere((element) =>
+                    element.songId.isEqual(pls.currentPlaylist[index].songId)),
+                setState(() {})
+              },
             ),
             IconButton(
-              onPressed: () => {},
-              icon: const Icon(Icons.offline_pin_rounded),
-            ),
-            IconButton(
-              onPressed: () => {},
-              icon: const Icon(Icons.delete_forever_outlined),
-            )
+                icon: const Icon(Icons.play_arrow),
+                color: Colors.white,
+                onPressed: () async =>
+                    {await pls.playIndexSong(index), setState(() {})}),
           ],
         ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: pls.currentPlaylist.length,
-            itemBuilder: (context, index) => ListTile(
-              dense: true,
-              onTap: () async =>
-                  {await pls.playIndexSong(index), setState(() {})},
-              leading: const Icon(Icons.playlist_add_check_circle),
-              trailing: pls.playIndex == index
-                  ? const Icon(Icons.headset_rounded)
-                  : null,
-              title: Text(
-                  pls.currentPlaylist.elementAt(index).title ?? "Unknow title"),
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+                child: Text(pls.currentPlaylist.elementAt(index).title ??
+                    "Unknow title")),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(Duration(
+                      seconds:
+                          pls.currentPlaylist.elementAt(index).song.duration)
+                  .toString()
+                  .split(".")
+                  .first),
             ),
-          ),
+            pls.playIndex == index
+                ? const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.headphones,
+                      color: Colors.white,
+                    ),
+                  )
+                : Container()
+          ],
         ),
-      ],
+      ),
     );
   }
 }
