33efb2fc2b48024a1c4c8809c71e2a00682e276d
ravachol nildock <ravacholnildock@devnull.mail>
2023-09-01 09:32:20 +0200

    refacto(main): Extract searchBar (wip)

---
+++ lib/components/search_bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SearchBarWidget extends StatefulWidget {
+  final String searchTarget;
+  const SearchBarWidget({required this.searchTarget, super.key});
+
+  @override
+  State<SearchBarWidget> createState() => _SearchBarWidgetState();
+}
+
+class _SearchBarWidgetState extends State<SearchBarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SearchAnchor(
+        suggestionsBuilder: (context, controller) {
+          // TODO(searchBar) : do full text search on db items
+          // var res = (db.artists.select()
+          //       ..where(
+          //           (tbl) => tbl.name.contains(controller.value.text)))
+          //     .watch();
+          return List<ListTile>.generate(5, (index) {
+            final String item = 'item $index for ${widget.searchTarget}';
+            return ListTile(
+              title: Text(item),
+              onTap: () {
+                setState(() {
+                  controller.closeView(item);
+                });
+              },
+            );
+          });
+        },
+        builder: (context, controller) {
+          return SearchBar(
+            controller: controller,
+            onTap: () {
+              controller.openView();
+            },
+            onChanged: (_) {
+              controller.openView();
+            },
+            padding: const MaterialStatePropertyAll<EdgeInsets>(
+                EdgeInsets.symmetric(horizontal: 16.0)),
+            leading: const Icon(Icons.search),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -86,7 +86,6 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _crawlMetarunning = false;
   final Map<String, bool> _bucketCrawling = {};
 
-  final pages = ["buckets", "artists", "albums", "songs", "playlist"];
   List<Widget> _widgets(Stream<List<MusicBucket>>? buckets,
           Stream<List<Artist>>? artists, Stream<List<Album>>? albums) =>
       [
@@ -298,7 +297,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   itemCount: snapshot.data!.length,
                   itemBuilder: (context, index) {
                     return ListTile(
-                      title: Text(snapshot.data![index].title),
+                      title: Text(
+                        snapshot.data![index].title,
+                        style: const TextStyle(color: Colors.white),
+                      ),
                       leading: Row(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -528,44 +530,6 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.start,
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          // Padding(
-          //   padding: const EdgeInsets.all(8.0),
-          //   child: SearchAnchor(
-          //     suggestionsBuilder: (context, controller) {
-          //       // TODO(main) : do full text search on db items
-          //       // var res = (db.artists.select()
-          //       //       ..where(
-          //       //           (tbl) => tbl.name.contains(controller.value.text)))
-          //       //     .watch();
-          //       return List<ListTile>.generate(5, (index) {
-          //         final String item =
-          //             'item $index for ${pages[_selectedIndex]}';
-          //         return ListTile(
-          //           title: Text(item),
-          //           onTap: () {
-          //             setState(() {
-          //               controller.closeView(item);
-          //             });
-          //           },
-          //         );
-          //       });
-          //     },
-          //     builder: (context, controller) {
-          //       return SearchBar(
-          //         controller: controller,
-          //         onTap: () {
-          //           controller.openView();
-          //         },
-          //         onChanged: (_) {
-          //           controller.openView();
-          //         },
-          //         padding: const MaterialStatePropertyAll<EdgeInsets>(
-          //             EdgeInsets.symmetric(horizontal: 16.0)),
-          //         leading: const Icon(Icons.search),
-          //       );
-          //     },
-          //   ),
-          // ),
           _widgets(buckets, artists, albums).elementAt(_selectedIndex),
           player.currentPlaylist.isEmpty ? Container() : PlayerWidget()
         ],
