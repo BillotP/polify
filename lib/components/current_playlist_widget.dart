25eb4576528e13cd712d0252741f729eaea43527
Paul Billot <paul1.billot@epitech.eu>
2023-11-25 19:09:18 +0100

    feat(currentplaylist):  search feature + favorite

--- lib/components/current_playlist_widget.dart
+++ lib/components/current_playlist_widget.dart
@@ -11,6 +11,7 @@ class CurrentPlaylistWidget extends StatefulWidget {
 
 class _CurrentPlaylistWidgetState extends State<CurrentPlaylistWidget> {
   PlayerService pls = Get.find();
+  // TODO(playlist): Finish save playlist implem
   // Row(
   //   mainAxisSize: MainAxisSize.max,
   //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -51,79 +52,112 @@ class _CurrentPlaylistWidgetState extends State<CurrentPlaylistWidget> {
         child: Text("Go listen to some music !"),
       );
     }
-    return ReorderableListView.builder(
-      buildDefaultDragHandles: false,
-      onReorder: (oldIndex, newIndex) {
-        setState(() {
-          if (oldIndex < newIndex) {
-            newIndex -= 1;
-          }
-          final SongToplay item = pls.currentPlaylist.removeAt(oldIndex);
-          pls.currentPlaylist.insert(newIndex, item);
-        });
-      },
-      itemCount: pls.currentPlaylist.length,
-      itemBuilder: (context, index) => ListTile(
-        // tileColor: Colors.orange,
-        dense: true,
-        trailing: ReorderableDragStartListener(
-            index: index,
-            child: const Icon(
-              Icons.drag_indicator_outlined,
-              color: Colors.white,
-            )),
-        key: Key(pls.currentPlaylist[index].songId.toString()),
-        leading: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.remove_circle),
-              color: Colors.white,
-              onPressed: () => {
-                pls.currentPlaylist.removeWhere((element) =>
-                    element.songId.isEqual(pls.currentPlaylist[index].songId)),
-                setState(() {})
-              },
+    return Column(
+      children: [
+        const DecoratedBox(
+          decoration: BoxDecoration(color: Colors.black),
+          child: ListTile(
+            title: Text(
+              "Filters",
+              style: TextStyle(color: Colors.white),
             ),
-            IconButton(
-                icon: const Icon(Icons.play_arrow),
-                color: Colors.white,
-                onPressed: () async =>
-                    {await pls.playIndexSong(index), setState(() {})}),
-          ],
+          ),
         ),
-        title: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Expanded(
-                child: Text(
-              pls.currentPlaylist.elementAt(index).title ?? "Unknow title",
-              style: const TextStyle(color: Colors.white),
-            )),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                Duration(
-                        seconds:
-                            pls.currentPlaylist.elementAt(index).song.duration)
-                    .toString()
-                    .split(".")
-                    .first,
-                style: const TextStyle(color: Colors.white),
+        Expanded(
+          child: ReorderableListView.builder(
+            buildDefaultDragHandles: false,
+            onReorder: (oldIndex, newIndex) {
+              setState(() {
+                if (oldIndex < newIndex) {
+                  newIndex -= 1;
+                }
+                final SongToplay item = pls.currentPlaylist.removeAt(oldIndex);
+                pls.currentPlaylist.insert(newIndex, item);
+              });
+            },
+            itemCount: pls.currentPlaylist.length,
+            itemBuilder: (context, index) => ListTile(
+              // tileColor: Colors.orange,
+              dense: true,
+              selected: index == pls.playIndex,
+              selectedTileColor: Colors.grey,
+              trailing: ReorderableDragStartListener(
+                  index: index,
+                  child: const Icon(
+                    Icons.drag_indicator_outlined,
+                    color: Colors.white,
+                  )),
+              key: Key(pls.currentPlaylist[index].songId.toString()),
+              leading: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.remove_circle),
+                    color: Colors.white,
+                    onPressed: () => {
+                      pls.currentPlaylist.removeWhere((element) => element
+                          .songId
+                          .isEqual(pls.currentPlaylist[index].songId)),
+                      setState(() {})
+                    },
+                  ),
+                  IconButton(
+                      icon: const Icon(Icons.play_arrow),
+                      color: Colors.white,
+                      onPressed: () async =>
+                          {await pls.playIndexSong(index), setState(() {})}),
+                ],
+              ),
+              title: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        pls.currentPlaylist[index].title,
+                        style: const TextStyle(color: Colors.white),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        pls.currentPlaylist[index].albumName,
+                        style: const TextStyle(color: Colors.white),
+                        overflow: TextOverflow.clip,
+                      ),
+                      Text(
+                        pls.currentPlaylist[index].artistName,
+                        style: const TextStyle(color: Colors.white),
+                        overflow: TextOverflow.clip,
+                      ),
+                    ],
+                  )),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      Duration(
+                              seconds: pls.currentPlaylist[index].song.duration)
+                          .toString()
+                          .split(".")
+                          .first,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  pls.playIndex == index
+                      ? const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.headphones,
+                            color: Colors.white,
+                          ),
+                        )
+                      : Container()
+                ],
               ),
             ),
-            pls.playIndex == index
-                ? const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Icon(
-                      Icons.headphones,
-                      color: Colors.white,
-                    ),
-                  )
-                : Container()
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
