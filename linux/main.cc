b5f1e77881e96413a98d75cd555de9a4d9518bc0
Paul Billot <paul1.billot@epitech.eu>
2023-11-28 12:32:59 +0100

    refacto(linux): debug for some segfault

--- linux/main.cc
+++ linux/main.cc
@@ -1,6 +1,12 @@
 #include "my_application.h"
+#include <cstdio>
 
-int main(int argc, char** argv) {
-  g_autoptr(MyApplication) app = my_application_new();
-  return g_application_run(G_APPLICATION(app), argc, argv);
+int main(int argc, char **argv) {
+  try {
+    g_autoptr(MyApplication) app = my_application_new();
+    return g_application_run(G_APPLICATION(app), argc, argv);
+  } catch (const char *&e) {
+    printf("Something bad happened : %s\n", e);
+    return 1;
+  }
 }
